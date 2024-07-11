9d3d83bc25cc7399aefbaaa1fb46bf48c71dc854
Tereza Kotlabova <terezka.kotlabova@gmail.com>
2024-07-11 17:37:46 +0200

    initial

---
+++ Sudoku/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    class Cell : Button
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        /// <summary>
+        /// Every cell is declared with Size, Location, Font and its style and size, FlatStyle and Flatappearance
+        /// </summary>
+        /// <param name="x"></ first cells coordinate>
+        /// <param name="y"></ second cells coordinate>
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+    
+            Size = new Size(45, 45);
+            Location = new Point(x * 45, y * 45);
+            Font = new Font("Arial", 23, FontStyle.Bold);
+            ForeColor = Color.Black;
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderColor = Color.Black;
+        }
+
+        public void ClearCell()
+        {
+            this.Text = string.Empty;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Sudoku/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateGrid();  
+        }
+
+        Cell[,] grid = new Cell[9, 9];
+        private void CreateGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    grid[i, j] = new Cell(i, j);
+
+                    //Make the squares 3x3 different colours
+                    grid[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? Color.LightSkyBlue : Color.CornflowerBlue;
+
+                    grid[i, j].KeyPress += ChangeValue;
+                    Grid.Controls.Add(grid[i, j]);
+                }
+            }
+        }
+
+        private void ChangeValue(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as Cell;
+            int value;
+
+            //Insert to cell only if pressed key is number
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            { 
+                cell.Text = value.ToString();
+                cell.ForeColor = Color.Black;
+            }   
+
+            //If pressed key is backspace, clear this cell
+            else if ((int)e.KeyChar == 8)
+            {
+                cell.ClearCell();
+            }
+        }
+    }
+}
